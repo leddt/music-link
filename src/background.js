ca4eda7b58892792d0d0536acc1db8b74f1006bc
David Thibault <david.thibault@sigmund.ca>
2023-05-09 22:46:43 -0400

    Working proof of concept

---
+++ src/background.js
@@ -0,0 +1,42 @@
+const targetService = "youtubeMusic";
+
+const domains = [
+    "https://open.spotify.com",
+    "https://music.apple.com"
+];
+
+const cache = {}
+
+chrome.tabs.onUpdated.addListener(async function(tabId, changeInfo, tab) {
+    if (changeInfo.status !== "loading") return;
+    if (!tab.url) return;
+    
+    if (!domains.find(x => tab.url.indexOf(x) == 0)) return;
+
+    try {
+        const data = await getLinks(tab.url);
+
+        if (data.linksByPlatform[targetService]) {
+            chrome.tabs.update(tabId, {
+                url: data.linksByPlatform[targetService].url
+            })
+        }
+    } catch {}
+})
+
+function getLinks(url) {
+    if (!cache[url]) {
+        console.log(`fetch ${url}`)
+        cache[url] = fetch(`https://api.song.link/v1-alpha.1/links?url=${url}`)
+            .then(response => {
+                if (response.status >= 400) {
+                    throw new Error("Links request failed")
+                }
+
+                return response.json();
+            })
+            .catch(() => delete cache[url]);
+    }
+
+    return cache[url]
+}
